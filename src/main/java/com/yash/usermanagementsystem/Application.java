1eb7d4eb8937f750cc753659ceb1121865cd17b2
Husain Burhanpurwala <high15915@gmail.com>
2025-06-03 12:54:08 +0530

    updated pom.xml

--- src/main/java/com/yash/usermanagementsystem/Application.java
+++ src/main/java/com/yash/usermanagementsystem/Application.java
@@ -7,7 +7,7 @@ import io.swagger.v3.oas.annotations.info.*;
 @OpenAPIDefinition(
     info = @Info(
             title = "UserManagementSystem",
-            version = "0.0"
+            version = "1.0"
     )
 )
 public class Application {
